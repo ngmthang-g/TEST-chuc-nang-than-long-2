be03f263b883db630074b45dcf8c5de494b04290
ngmthang-g <hyanrymine3@gmail.com>
2026-08-16 02:33:37 +0700

    fix(test): apply UI resolver inside bridge part

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -1,11 +1,6 @@
 #include "bridge_part01.inc"
 #include "bridge_part02.inc"
-#define FindUiRootViaPublicApi FindUiRootViaPublicApi_v110
-#define FindUiRoot FindUiRoot_v110
 #include "bridge_part03.inc"
-#undef FindUiRoot
-#undef FindUiRootViaPublicApi
-#include "bridge_ui_root_fix_v1_1_2.inc"
 #include "bridge_part04.inc"
 #include "bridge_part05.inc"
 #include "bridge_part06.inc"
