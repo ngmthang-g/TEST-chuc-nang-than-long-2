05201903b27fb9d5960ce27fa07e8046d079e18d
ngmthang-g <hyanrymine3@gmail.com>
2026-08-16 03:23:57 +0700

    fix(test): route heal choices through v1.1.4 semantic packet layer

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -1,7 +1,17 @@
+#include <cstddef>
+#include "protocol.h"
+
+namespace {
+bool ClickHealDialogChoice(cleanroute::HealDialogChoice choice, wchar_t* detail, std::size_t cap);
+}
+
 #include "bridge_part01.inc"
 #include "bridge_lua_array.inc"
 #include "bridge_part02.inc"
 #include "bridge_part03.inc"
+#define ClickHealDialogChoice ClickHealDialogChoiceV113
 #include "bridge_part04.inc"
+#undef ClickHealDialogChoice
 #include "bridge_part05.inc"
 #include "bridge_part06.inc"
+#include "bridge_heal_packet_v1_1_4.inc"
