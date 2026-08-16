7df41327826e90e855c08c3b545030d438ea732c
ngmthang-g <hyanrymine3@gmail.com>
2026-08-16 10:52:29 +0700

    v1.1.7: update controller title

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,4 +1,7 @@
+#define kTitle kTitleLegacyV116
 #include "controller_part01.inc"
+#undef kTitle
+constexpr wchar_t kTitle[] = L"Thần Long TEST v1.1.7 • Auto trị liệu • Dialog observer";
 #include "controller_part02.inc"
 #include "controller_part03.inc"
 #include "controller_part04.inc"
